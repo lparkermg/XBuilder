88e9c196c06a1b5e3fad3770fbe6cb00c258ee21
Luke Parker <moongateuk@gmail.com>
2016-08-03 20:45:55 +0100

    Started work on builder, added unit testing. Started work on how the builder will be layed out. Added unit testing to the solution using NUnit.

--- src/BuildXDoc/Builder.cs
+++ src/BuildXDoc/Builder.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BuildXDoc
 {
     public class Builder
     {
+        public XDocument XmlData { get; private set; }
+        public string RootName { get; private set; }
 
+        public Builder(string key, List<KeyValuePair<string, string>> attributes = null)
+        {
+            List<object> attr = new List<object>();
+            RootName = key;
+
+            if (attributes != null)
+            {
+                foreach (var attribute in attributes)
+                {
+                    attr.Add(new XAttribute(attribute.Key,attribute.Value));
+                }
+            }
+            
+            XmlData = new XDocument(new XElement(key, attr.ToArray()));
+        }
     }
 }
---
+++ src/Builder.Tests/BuilderValidate.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Builder = BuildXDoc.Builder;
+
+namespace BuilderTests
+{
+    [TestFixture]
+    public class BuilderValidate
+    {
+        private Builder _builder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _builder = new Builder("TestRootElement");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _builder = null;
+        }
+
+        [Test]
+        public void New_Builder_Should_Start_With_One_Root_Element()
+        {
+            var data = _builder.XmlData.Elements().ToList();
+
+            Assert.AreEqual(1,data.Count());
+        }
+
+        [Test]
+        public void New_Builder_Root_Element_Should_Have_Correct_LocalName()
+        {
+            var data = _builder.XmlData.Elements().First();
+
+            Assert.AreEqual("TestRootElement",data.Name.LocalName);
+        }
+
+        [Test]
+        public void New_Builder_Should_Support_Multiple_Attributes()
+        {
+            var attrs = new List<KeyValuePair<string, string>> ()
+            {
+                new KeyValuePair<string, string>("attrOne", "Value One"),
+                new KeyValuePair<string, string>("attrTwo", "Another value")
+            };
+            _builder = new Builder("TestRootElement",attrs);
+
+            var data = _builder.XmlData.Elements().First();
+            var attrCount = data.Attributes().Count();
+
+            Assert.AreEqual(2,attrCount);
+        }
+    }
+}
