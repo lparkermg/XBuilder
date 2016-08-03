a964a418d3d64b670ecde602ce7a8af2a307b4be
Luke Parker <moongateuk@gmail.com>
2016-08-03 23:44:16 +0100

    Built unit tests for builder, started Add function Have somewhat indepth unit testing for adding elements. Started work on the Add function (now adds duplicates X amount of times.

--- src/BuildXDoc/Builder.cs
+++ src/BuildXDoc/Builder.cs
@@ -27,5 +27,81 @@ namespace BuildXDoc
             
             XmlData = new XDocument(new XElement(key, attr.ToArray()));
         }
+
+        //TODO:If more than one node in list, make selectable (i.e pass through a number to select.)
+        public void Add(string elementName, int amount = 1, string toWhere = "")
+        {
+            if (string.IsNullOrWhiteSpace(toWhere))
+                toWhere = RootName;
+
+            var ele = XmlData.Elements().First(e => e.Name.LocalName == toWhere);
+
+            for (var i = 0; i < amount; i++)
+            {
+                ele.Add(new XElement(elementName));
+            }
+        }
+
+        //TODO:If more than one node in list, make selectable (i.e pass through a number to select.)
+        public void Add(string elementName, string value, int amount = 1, string toWhere = "")
+        {
+            if (string.IsNullOrWhiteSpace(toWhere))
+                toWhere = RootName;
+
+            var ele = XmlData.Elements().First(e => e.Name.LocalName == toWhere);
+
+            for (var i = 0; i < amount; i++)
+            {
+                ele.Add(new XElement(elementName, value));
+            }
+        }
+
+        //TODO:If more than one node in list, make selectable (i.e pass through a number to select.)
+        public void Add(string elementName, List<KeyValuePair<string, string>> attributes, int amount = 1,
+            string toWhere = "")
+        {
+            if (string.IsNullOrWhiteSpace(toWhere))
+                toWhere = RootName;
+
+            var attrs = new List<XAttribute>();
+
+            foreach (var attr in attributes)
+            {
+                attrs.Add(new XAttribute(attr.Key,attr.Value));
+            }
+
+            var ele = XmlData.Elements().First(e => e.Name.LocalName == toWhere);
+            var attrsArray = attrs.ToArray();
+
+            for (var i = 0; i < amount; i++)
+            {
+                
+                ele.Add(new XElement(elementName,attrsArray));
+            }
+        }
+
+        //TODO:If more than one node in list, make selectable (i.e pass through a number to select.)
+        public void Add(string elementName, string value, List<KeyValuePair<string, string>> attributes, int amount = 1,
+            string toWhere = "")
+        {
+            if (string.IsNullOrWhiteSpace(toWhere))
+                toWhere = RootName;
+
+            var attrs = new List<XAttribute>();
+
+            foreach (var attr in attributes)
+            {
+                attrs.Add(new XAttribute(attr.Key,attr.Value));
+            }
+
+            
+            var ele = XmlData.Elements().First(e => e.Name.LocalName == toWhere);
+            var attrsArray = attrs.ToArray();
+
+            for (var i = 0; i < amount; i++)
+            {
+                ele.Add(new XElement(elementName, value,attrsArray));
+            }
+        }
     }
 }
--- src/Builder.Tests/BuilderValidate.cs
+++ src/Builder.Tests/BuilderValidate.cs
@@ -22,6 +22,7 @@ namespace BuilderTests
             _builder = null;
         }
 
+        #region Builder Init
         [Test]
         public void New_Builder_Should_Start_With_One_Root_Element()
         {
@@ -53,5 +54,85 @@ namespace BuilderTests
 
             Assert.AreEqual(2,attrCount);
         }
+        #endregion
+
+        #region Adding Elements (Basic)
+
+        [Test]
+        public void Builder_Should_Be_Able_To_Add_Without_Value_In_Element()
+        {
+            _builder.Add("NodeUnderRoot");
+
+            var ele = _builder.XmlData.Elements().First(e => e.Name.LocalName == "TestRootElement");
+            var node = ele.Elements().First(e => e.Name.LocalName == "NodeUnderRoot");
+            Assert.AreEqual("NodeUnderRoot",node.Name.LocalName);
+        }
+
+        [Test]
+        public void Builder_Should_Be_Able_To_Add_With_Value_In_Element()
+        {
+            _builder.Add("NodeUnderRoot", "Node Value");
+
+            var ele = _builder.XmlData.Elements().First(e => e.Name.LocalName == "TestRootElement");
+            var val = ele.Elements().First(e => e.Name.LocalName == "NodeUnderRoot").Value;
+
+            Assert.AreEqual("Node Value", val);
+        }
+
+        [Test]
+        public void Builder_Should_Be_Able_To_Add_With_Multiple_Attributes_In_Element()
+        {
+            var attrs = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("attrOne", "Attribute Value One."),
+                new KeyValuePair<string, string>("attrTwo", "Attribute Value Two")
+            };
+
+            _builder.Add("NodeUnderRoot", attrs);
+
+            var ele = _builder.XmlData.Elements().First(e => e.Name.LocalName == "TestRootElement");
+            var node = ele.Elements().First(e => e.Name.LocalName == "NodeUnderRoot");
+
+            var attrCount = node.Attributes().Count();
+
+            Assert.AreEqual(2,attrCount);
+        }
+
+        [Test]
+        public void Builder_Should_Be_Able_To_Add_With_Value_And_Attributes_In_Element()
+        {
+            var attrs = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("attrOne", "Attribute Value One."),
+                new KeyValuePair<string, string>("attrTwo", "Attribute Value Two")
+            };
+
+            _builder.Add("NodeUnderRoot", "Node Value", attrs);
+
+            var ele = _builder.XmlData.Elements().First(e => e.Name.LocalName == "TestRootElement");
+            var node = ele.Elements().First(e => e.Name.LocalName == "NodeUnderRoot");
+
+            var val = node.Value;
+            var attrCount = node.Attributes().Count();
+
+            Assert.AreEqual("Node Value",val);
+            Assert.AreEqual(2, attrCount);
+        }
+
+        [Test]
+        public void Builder_Should_Be_Able_To_Add_X_Amount_Of_Duplicates_In_Element()
+        {
+            var amount = 10;
+
+            _builder.Add("NodeUnderRoot", amount);
+
+            var ele = _builder.XmlData.Elements().First(e => e.Name.LocalName == "TestRootElement");
+            var nodeCount = ele.Elements().Where(e => e.Name.LocalName == "NodeUnderRoot").ToList().Count;
+
+            Assert.AreEqual(amount, nodeCount);
+        }
+
+
+        #endregion
     }
 }
